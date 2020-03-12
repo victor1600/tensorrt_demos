18c905793c9b2d38faccce90c3cd0af2f6b015a1
victor1600 <43228121+victor1600@users.noreply.github.com>
2020-03-12 15:09:36 -0600

    Update trt_yolov3.py

--- trt_yolov3.py
+++ trt_yolov3.py
@@ -1,95 +1,47 @@
-"""trt_yolov3.py
-
-This script demonstrates how to do real-time object detection with
-TensorRT optimized YOLOv3 engine.
-"""
-
-
-import sys
-import time
+# import the necessary packages
 import argparse
-
-import cv2
-import pycuda.autoinit  # This is needed for initializing CUDA driver
-
-from utils.yolov3_classes import get_cls_dict
-from utils.yolov3 import TrtYOLOv3
-from utils.camera import add_camera_args, Camera
-from utils.display import open_window, set_display, show_fps
-from utils.visualization import BBoxVisualization
-
-
-WINDOW_NAME = 'TrtYOLOv3Demo'
+import random
+import os
 
 
 def parse_args():
-    """Parse input arguments."""
-    desc = ('Capture and display live camera video, while doing '
-            'real-time object detection with TensorRT optimized '
-            'YOLOv3 model on Jetson Nano')
-    parser = argparse.ArgumentParser(description=desc)
-    parser = add_camera_args(parser)
-    parser.add_argument('--model', type=str, default='yolov3-416',
-                        choices=['yolov3-288', 'yolov3-416', 'yolov3-608',
-                                 'yolov3-tiny-288', 'yolov3-tiny-416'])
-    args = parser.parse_args()
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-s", "--shuffle", required=False,
+                    help="shuffle the input images", default="False")
+    ap.add_argument('-p', "--path", required=True,
+                    help="path to input images")
+    args = vars(ap.parse_args())
     return args
 
 
-def loop_and_detect(cam, trt_yolov3, conf_th, vis):
-    """Continuously capture images from camera and do object detection.
+def create_subdir(folders):
+    for folder in folders:
+        os.system(f'mkdir test/{folder}')
 
-    # Arguments
-      cam: the camera instance (video source).
-      trt_yolov3: the TRT YOLOv3 object detector instance.
-      conf_th: confidence/score threshold for object detection.
-      vis: for visualization.
-    """
-    full_scrn = False
-    fps = 0.0
-    tic = time.time()
-    while True:
-        if cv2.getWindowProperty(WINDOW_NAME, 0) < 0:
-            break
-        img = cam.read()
-        if img is not None:
-            boxes, confs, clss = trt_yolov3.detect(img, conf_th)
-            img = vis.draw_bboxes(img, boxes, confs, clss)
-            img = show_fps(img, fps)
-            cv2.imshow(WINDOW_NAME, img)
-            toc = time.time()
-            curr_fps = 1.0 / (toc - tic)
-            # calculate an exponentially decaying average of fps number
-            fps = curr_fps if fps == 0.0 else (fps*0.95 + curr_fps*0.05)
-            tic = toc
-        key = cv2.waitKey(1)
-        if key == 27:  # ESC key: quit program
-            break
-        elif key == ord('F') or key == ord('f'):  # Toggle fullscreen
-            full_scrn = not full_scrn
-            set_display(WINDOW_NAME, full_scrn)
+
+def move_images(classes, args):
+    for className in classes:
+        counter= 0
+        images = os.listdir(f'{args["path"]}/{className}/')
+        testDatasetSize = round(len(images) * 0.20)
+        if args['shuffle'] == "True":
+            images = random.sample(images, len(images))
+
+        for image in images:
+            if counter < testDatasetSize:
+                os.rename(f'{args["path"]}/{className}/{image}', f'test/{className}/{image}')
+                counter += 1
+        print(f'[{className}] test images are ready.')
+    
+    os.system(f'mv {args["path"]} train')
 
 
 def main():
     args = parse_args()
-    cam = Camera(args)
-    cam.open()
-    if not cam.is_opened:
-        sys.exit('Failed to open camera!')
-
-    cls_dict = get_cls_dict('coco')
-    yolo_dim = int(args.model.split('-')[-1])  # 416 or 608
-    trt_yolov3 = TrtYOLOv3(args.model, (yolo_dim, yolo_dim))
-
-    cam.start()
-    open_window(WINDOW_NAME, args.image_width, args.image_height,
-                'Camera TensorRT YOLOv3 Demo')
-    vis = BBoxVisualization(cls_dict)
-    loop_and_detect(cam, trt_yolov3, conf_th=0.3, vis=vis)
-
-    cam.stop()
-    cam.release()
-    cv2.destroyAllWindows()
+    os.system('mkdir test')
+    directories = os.listdir(args['path'])
+    create_subdir(directories)
+    move_images(directories, args)
 
 
 if __name__ == '__main__':
